dfb598cef6599b5bddcf8f13caa59a617123c3d9
vivishno <64971172+vivishno@users.noreply.github.com>
2020-06-23 01:06:59 +0530

    Update main.py

--- code/main.py
+++ code/main.py
@@ -123,7 +123,7 @@ def main():
     deployment_async_operation = client.deployments.create_or_update(
             resource_group,
             'azure-sample',
-           parameters=deployment_properties
+           parameters=deployment_properties  
         )
     deployment_async_operation.wait()
     #if success:
