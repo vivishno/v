61eb23728ca8aea3fa32b1fb36fc56fc17ae51a1
vivishno <64971172+vivishno@users.noreply.github.com>
2020-06-23 10:19:01 +0530

    Update main.py

--- code/main.py
+++ code/main.py
@@ -117,13 +117,13 @@ def main():
      }
     print("---------------------checking properties----------------------------")
     #p=DeploymentProperties(DeploymentMode.incremental, template=template1,  parameters=parameters1)
-    deploy_parameter =Deployment(properties=deployment_properties)
+    #deploy_parameter =Deployment(properties=deployment_properties)
     print(client.deployments)
     print(parameters)
     deployment_async_operation = client.deployments.create_or_update(
             resource_group,
             'azure-sample',
-            deploy_parameter
+            deployment_properties
         )
     deployment_async_operation.wait()
     #if success:
